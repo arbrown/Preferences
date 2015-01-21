25d66252e9b44eba4bc08afe39a4812fbdf0adb7
Andrew Brown <andrewrbrown1@gmail.com>
2015-01-20 19:55:45 -0700

    First pass at role assignment seems to be working
    Simple example attempts to assign a preferred role to players and
    returns assignments along with the "weight" of this optimal solution
    (best = 5, worst = 25)

--- main.go
+++ main.go
@@ -20,14 +20,101 @@ type RoleAssignment struct {
 }
 
 // Assign roles to a group of players
-func AssignRoles(prefs []RolePreference) ([]RoleAssignment, error) {
+func AssignRoles(prefs []RolePreference) ([]RoleAssignment, int, error) {
 	if len(prefs) != 5 {
-		return nil, errors.New("Need 5 players to assign roles")
+		return nil, 25, errors.New("Need 5 players to assign roles")
 	}
 
 	// We have 5 players, refer to them by their slice index
 	// p1 = prefs[0] etc...
-	return nil, nil
+
+	bestWeight := 25 // worst case, everyone gets least favorite
+	bestSolutions := make([][]int, 1)
+
+	for current := range GeneratePermutations([]int{1, 2, 3, 4, 5}) {
+		currentWeight, _ := GetWeight(current, prefs)
+		if currentWeight <= bestWeight {
+			if currentWeight < bestWeight {
+				bestWeight = currentWeight
+				bestSolutions = nil
+			}
+			sliceCopy := make([]int, len(current))
+			copy(sliceCopy, current)
+			bestSolutions = append(bestSolutions, sliceCopy)
+		}
+	}
+
+	// for now, just take the first, best solution
+	if len(bestSolutions) == 0 {
+		return nil, 25, errors.New("Could not find any valid solutions")
+	}
+
+	bestSolution := bestSolutions[0]
+	assignments := make([]RoleAssignment, len(prefs))
+
+	for i, p := range prefs {
+		name, _ := GetRoleName(bestSolution[i])
+		assignments[i] = RoleAssignment{Name: p.Name, Role: name}
+	}
+
+	return assignments, bestWeight, nil
+}
+
+func GetRoleName(i int) (string, error) {
+	// 1 - Trapper
+	// 2 - Medic
+	// 3 - Support
+	// 4 - Assault
+	// 5 - Monster
+	switch i {
+	case 1:
+		return "Trapper", nil
+	case 2:
+		return "Medic", nil
+	case 3:
+		return "Support", nil
+	case 4:
+		return "Assault", nil
+	case 5:
+		return "Monster", nil
+	}
+
+	return "", errors.New("Role not found")
+}
+
+// Get the preference weight for this role permutation
+func GetWeight(current []int, prefs []RolePreference) (int, error) {
+	sum := 0
+	for i := 0; i < 5; i++ {
+		if w, e := GetRoleWeight(current[i], prefs[i]); e == nil {
+			sum += w
+		} else {
+			return 25, e
+		}
+	}
+	return sum, nil
+}
+
+// Get an individual player's preference for his given role
+func GetRoleWeight(role int, playerPreferences RolePreference) (int, error) {
+	// 1 - Trapper
+	// 2 - Medic
+	// 3 - Support
+	// 4 - Assault
+	// 5 - Monster
+	switch role {
+	case 1:
+		return playerPreferences.Trapper, nil
+	case 2:
+		return playerPreferences.Medic, nil
+	case 3:
+		return playerPreferences.Support, nil
+	case 4:
+		return playerPreferences.Assault, nil
+	case 5:
+		return playerPreferences.Monster, nil
+	}
+	return -1, errors.New("Role not found")
 }
 
 // Return every permutation of integers in a slice
@@ -82,8 +169,16 @@ func GeneratePermutations(values []int) <-chan []int {
 }
 
 func main() {
-
-	for combo := range GeneratePermutations([]int{1, 2, 3, 4, 5}) {
-
+	sample := []RolePreference{
+		RolePreference{1, 2, 3, 4, 5, "Vincent"},
+		RolePreference{5, 4, 3, 2, 1, "Edgar"},
+		RolePreference{1, 5, 2, 4, 3, "Lenn0s"},
+		RolePreference{1, 2, 3, 4, 5, "Perlmonger42"},
+		RolePreference{1, 4, 2, 3, 5, "Walrus"},
 	}
+
+	result, weight, _ := AssignRoles(sample)
+
+	fmt.Println(result)
+	fmt.Printf("Weight: %d\n", weight)
 }
